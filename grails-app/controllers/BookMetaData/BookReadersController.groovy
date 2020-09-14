6f66a9100f5e857a02c9b0de9e51848945f4d15d
Sankalp <sankalp1468@gmail.com>
2020-09-13 23:33:16 -0700

    More on Tracker

--- grails-app/controllers/BookMetaData/BookReadersController.groovy
+++ grails-app/controllers/BookMetaData/BookReadersController.groovy
@@ -1,24 +1,43 @@
 package BookMetaData
 
+import com.amazonaws.util.json.JSONObject
+import grails.converters.deep.JSON
+import org.json.simple.JSONObject
+
 class BookReadersController {
     
     static allowedMethods = [saveUser: "POST", update: "POST", delete: "POST"]
 
     def index() { }
     
-    def bookTracker(){
+    def readBook(){
+        params.bookId
+        params.email
+        
+        def oui = BooksProfile.findByOldUserList()
+        if(!oui){
+            newUserList.add(params.email)
+            [newUserList:newUserList]
+        }else{
+            print "user has already read the book"
+        }
         
     }
     
-    Long getCurrentBookCount(){
-        
+    def getCurrentBookActiveCount(){
+        def count = BooksProfile.findByBookId()
+        def resCount = new JSONObject(activeUsers.newUserList);
+        render ([status:1,message:'success',data: resCount] as JSON)
+        [count:count]
     }
     
-    String[] getCurrentBookUsers(){
-        
+    def getCurrentBookActiveUsers(){
+        def activeUsers = BooksProfile.findByBookId(params.bookId)
+        def resUser = new JSONObject(activeUsers.newUserList);
+        render ([status:1,message:'success',data: resUser] as JSON)
     }
     
-    Long getOldBookUsers(){
+    def getOldBookUsers(){
         
     }
 }
